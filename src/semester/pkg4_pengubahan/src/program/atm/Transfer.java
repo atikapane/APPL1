d18b07a32e54b3a09c521bb5d5756082531e044c
Tiara Lestari S <tiaralestaris@yahoo.com>
2019-03-08 08:31:52 +0700

    Perubahan Class Transfer
    Penerapan transfer limit berdasarkan jenis account, belum dilakukan pengecekan date

--- src/semester/pkg4_pengubahan/src/program/atm/Transfer.java
+++ src/semester/pkg4_pengubahan/src/program/atm/Transfer.java
@@ -3,35 +3,43 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package semester.pkg4_pengubahan.src.program.atm;
 
 class Transfer {
-  private final int numFrom;
-  private final int numTo;
-  private final double value;
-  private BankDatabase bankDatabase;
-  
-  Transfer(BankDatabase bankDatabase, int numFrom, int numTo, double value) {
-    this.bankDatabase = bankDatabase;
-    this.numFrom = numFrom;
-    this.numTo = numTo;
-    this.value = value;
-  }
-  
-  public void execute() {
-    assert(numFrom != numTo);
-    Account accFrom = bankDatabase.getAccount(numFrom);
-    Account accTo = bankDatabase.getAccount(numTo);
-    assert(accFrom != null);
-    assert(accTo != null);
-    if(accFrom.getJenis() == 2){
-        if(accFrom.getTransferLimit() < 10000){
-            
-        }
+
+    private final int numFrom;
+    private final int numTo;
+    private final double value;
+    private BankDatabase bankDatabase;
+
+    Transfer(BankDatabase bankDatabase, int numFrom, int numTo, double value) {
+        this.bankDatabase = bankDatabase;
+        this.numFrom = numFrom;
+        this.numTo = numTo;
+        this.value = value;
     }
-    accFrom.setAvailableBalance(accFrom.getAvailableBalance() - value);
-    accFrom.setTotalBalance(accFrom.getTotalBalance() - value);
-    accTo.setTotalBalance(accTo.getTotalBalance() + value);
-  }
+
+    public void execute() {
+        Screen screen = new Screen();
+        assert (numFrom != numTo);
+        Account accFrom = bankDatabase.getAccount(numFrom);
+        Account accTo = bankDatabase.getAccount(numTo);
+        assert (accFrom != null);
+        assert (accTo != null);
+        if (accFrom.getJenis() == 2) {
+            if (accFrom.getTransferLimit() < 10000) {
+                accFrom.setTransferLimit(value);
+                accFrom.setAvailableBalance(accFrom.getAvailableBalance() - value);
+                accFrom.setTotalBalance(accFrom.getTotalBalance() - value);
+                accTo.setTotalBalance(accTo.getTotalBalance() + value);
+            }else screen.displayMessageLine("\nYou have reach your transfer limit.");
+        } else if (accFrom.getJenis() == 3) {
+            if (accFrom.getTransferLimit() < 500) {
+                accFrom.setTransferLimit(value);
+                accFrom.setAvailableBalance(accFrom.getAvailableBalance() - value);
+                accFrom.setTotalBalance(accFrom.getTotalBalance() - value);
+                accTo.setTotalBalance(accTo.getTotalBalance() + value);
+            }else screen.displayMessageLine("\nYou have reach your transfer limit.");
+        } else screen.displayMessageLine("Your account cannot do this action");
+    }
 }
