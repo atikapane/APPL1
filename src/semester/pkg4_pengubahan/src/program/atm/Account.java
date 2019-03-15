d790ea6eb0af0fa80284531b8310359bf7fc8ec5
danju27 <danju27@github.com>
2019-03-15 07:56:51 +0700

    menyelesaikan task (d,e,f)
    pengerjaan yang saya kerjakan sudah di merge lewat branch tiara

--- src/semester/pkg4_pengubahan/src/program/atm/Account.java
+++ src/semester/pkg4_pengubahan/src/program/atm/Account.java
@@ -3,10 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package semester.pkg4_pengubahan.src.program.atm;
 
 public class Account {
+<<<<<<< HEAD
    private int accountNumber; // account number
    private int pin; // PIN for authentication
    private double availableBalance; // funds available for withdrawal
@@ -14,10 +14,101 @@ public class Account {
    private boolean blocked;
    private int monthlyFeeStatus; 
    int jenis;
+=======
+
+    /**
+     * @return the jenis
+     */
+    private int accountNumber; // account number
+    private int pin; // PIN for authentication
+    private double availableBalance; // funds available for withdrawal
+    private double totalBalance; // funds available & pending deposits
+    private boolean blocked;
+    private int jenis;
+>>>>>>> master
 //   private int SISWA = 1;
 //   private int BISNIS = 2;
 //   private int MASA_DEPAN = 3;
+    private double transferLimit = 0;
 
+    // Account constructor initializes attributes
+    public Account(int theAccountNumber, int thePIN,
+            double theAvailableBalance, double theTotalBalance, int tipe) {
+        this.accountNumber = theAccountNumber;
+        this.availableBalance = theAvailableBalance;
+        this.totalBalance = theTotalBalance;
+        this.pin = thePIN;
+        this.blocked = false;
+
+        //tipe == 1 then siswa
+        //tipe == 2 then bisnis
+        //tipe == 3 then masa_depan
+        this.jenis = tipe;
+
+    }
+
+    // determines whether a user-specified PIN matches PIN in Accountsss
+    public boolean validatePIN(int userPIN) {
+        if (userPIN == pin) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // returns available balance
+    public double getAvailableBalance() {
+        return availableBalance;
+    }
+
+    // returns the total balance
+    public double getTotalBalance() {
+        return totalBalance;
+    }
+
+    public int getJenis() {
+        return this.jenis;
+    }
+
+    public double getTransferLimit() {
+        return this.transferLimit;
+    }
+
+    public void setTransferLimit(double x) {
+        this.transferLimit += x;
+    }
+
+    public void setAvailableBalance(double x) {
+        this.availableBalance = x;
+    }
+
+    public void setTotalBalance(double x) {
+        this.totalBalance = x;
+    }
+
+    public void credit(double amount) {
+        this.availableBalance -= amount;
+        this.totalBalance -= amount;
+    }
+
+    public void debit(double amount) {
+        this.availableBalance += amount;
+        this.totalBalance += amount;
+    }
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public void setPIN(int pinToSet) {
+        this.pin = pinToSet;
+    }
+
+    public boolean isBlocked() {
+        return blocked;
+    }
+
+<<<<<<< HEAD
    // Account constructor initializes attributes
    public Account(int theAccountNumber, int thePIN, 
       double theAvailableBalance, double theTotalBalance, int tipe, int atmMonthlyFeeStatus) {
@@ -105,3 +196,9 @@ public class Account {
         this.monthlyFeeStatus = monthlyFeeStatus;
     }
 } 
+=======
+    public void setBlocked(boolean value) {
+        blocked = value;
+    }
+}
+>>>>>>> master
